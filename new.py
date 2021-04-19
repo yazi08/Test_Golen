3714c2fcd378ac4e6f1200d176f6a3b6206ef5e0
yazi08 <yaroslav24.01.1998@mail.ru>
2021-04-19 21:05:14 +0300

    Update new.py
    добавил свое имя

--- new.py
+++ new.py
@@ -1,1 +1,12 @@
 
+print("Hello Yar")
+class Myname:
+    def __init__(self,name):
+        self.name = name
+        
+    def __call__(self):
+        return name
+        
+my = Myname()
+my = Myname("Yazi")
+my.name
